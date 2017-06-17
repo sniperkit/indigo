06b024c821ad1f220b4143ed69c98f445f6b6aad
Minoru Osuka <minoru.osuka@gmail.com>
2017-06-17 18:34:55 +0900

    refactor indigo comannd

--- indigo/cmd/root.go
+++ indigo/cmd/root.go
@@ -15,11 +15,19 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
-	ver "github.com/mosuka/indigo/version"
+	"github.com/blevesearch/bleve/mapping"
+	"github.com/mosuka/indigo/indigo/grpc"
+	"github.com/mosuka/indigo/version"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"io/ioutil"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 type RootCommandOptions struct {
@@ -28,26 +36,169 @@ type RootCommandOptions struct {
 
 var rootCmdOpts RootCommandOptions
 
+var logOutput *os.File
+
 var RootCmd = &cobra.Command{
-	Use:               "indigo",
-	Short:             "CLI for Indigo Server",
-	Long:              `The Command Line Interface for the Indigo Server.`,
-	PersistentPreRunE: persistentPreRunERootCmd,
-	RunE:              runERootCmd,
+	Use:                "indigo",
+	Short:              "CLI for Indigo Server",
+	Long:               `The Command Line Interface for the Indigo Server.`,
+	PersistentPreRunE:  persistentPreRunERootCmd,
+	RunE:               runERootCmd,
+	PersistentPostRunE: persistentPostRunERootCmd,
 }
 
 func persistentPreRunERootCmd(cmd *cobra.Command, args []string) error {
 	if rootCmdOpts.versionFlag {
-		fmt.Printf("%s\n", ver.Version)
+		fmt.Printf("%s\n", version.Version)
 		os.Exit(0)
+	}
+
+	switch viper.GetString("log_format") {
+	case "text":
+		log.SetFormatter(&log.TextFormatter{
+			ForceColors:      false,
+			DisableColors:    true,
+			DisableTimestamp: false,
+			FullTimestamp:    true,
+			TimestampFormat:  time.RFC3339,
+			DisableSorting:   false,
+			QuoteEmptyFields: true,
+			QuoteCharacter:   "\"",
+		})
+	case "color":
+		log.SetFormatter(&log.TextFormatter{
+			ForceColors:      true,
+			DisableColors:    false,
+			DisableTimestamp: false,
+			FullTimestamp:    true,
+			TimestampFormat:  time.RFC3339,
+			DisableSorting:   false,
+			QuoteEmptyFields: true,
+			QuoteCharacter:   "\"",
+		})
+	case "json":
+		log.SetFormatter(&log.JSONFormatter{
+			TimestampFormat:  time.RFC3339,
+			DisableTimestamp: false,
+			FieldMap: log.FieldMap{
+				log.FieldKeyTime:  "@timestamp",
+				log.FieldKeyLevel: "@level",
+				log.FieldKeyMsg:   "@message",
+			},
+		})
+	default:
+		log.SetFormatter(&log.TextFormatter{
+			ForceColors:      false,
+			DisableColors:    true,
+			DisableTimestamp: false,
+			FullTimestamp:    true,
+			TimestampFormat:  time.RFC3339,
+			DisableSorting:   false,
+			QuoteEmptyFields: true,
+			QuoteCharacter:   "\"",
+		})
+	}
+
+	switch viper.GetString("log_level") {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	case "fatal":
+		log.SetLevel(log.FatalLevel)
+	case "panic":
+		log.SetLevel(log.PanicLevel)
+	default:
+		log.SetLevel(log.InfoLevel)
+	}
+
+	if viper.GetString("log_output") == "" {
+		log.SetOutput(os.Stdout)
+	} else {
+		var err error
+		logOutput, err = os.OpenFile(viper.GetString("log_output"), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			return err
+		} else {
+			log.SetOutput(logOutput)
+		}
 	}
 
 	return nil
 }
 
 func runERootCmd(cmd *cobra.Command, args []string) error {
-	if len(args) < 1 {
-		return cmd.Help()
+	// IndexMapping
+	indexMapping := mapping.NewIndexMapping()
+	if cmd.Flag("index-mapping").Changed {
+		file, err := os.Open(viper.GetString("index_mapping"))
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		resourceBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			return err
+		}
+
+		err = json.Unmarshal(resourceBytes, indexMapping)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Kvconfig
+	kvconfig := make(map[string]interface{})
+	if cmd.Flag("kvconfig").Changed {
+		file, err := os.Open(viper.GetString("kvconfig"))
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		resourceBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			return err
+		}
+
+		err = json.Unmarshal(resourceBytes, &kvconfig)
+		if err != nil {
+			return err
+		}
+	}
+
+	server := grpc.NewIndigoGRPCServer(viper.GetInt("port"), viper.GetString("path"), indexMapping, viper.GetString("index_type"), viper.GetString("kvstore"), kvconfig)
+	server.Start(viper.GetBool("delete_index_at_startup"))
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	for {
+		sig := <-signalChan
+
+		log.WithFields(log.Fields{
+			"signal": sig,
+		}).Info("trap signal")
+
+		server.Stop(viper.GetBool("delete_index_at_shutdown"))
+
+		return nil
+	}
+
+	return nil
+}
+
+func persistentPostRunERootCmd(cmd *cobra.Command, args []string) error {
+	if viper.GetString("log_output") != "" {
+		logOutput.Close()
 	}
 
 	return nil
@@ -85,8 +236,31 @@ func LoadConfig() {
 func init() {
 	cobra.OnInitialize(LoadConfig)
 
-	RootCmd.PersistentFlags().String("config", DefaultConfig, "configuration file path")
 	RootCmd.PersistentFlags().BoolVar(&rootCmdOpts.versionFlag, "version", false, "show version number")
+	RootCmd.PersistentFlags().String("config", DefaultConfig, "configuration file path")
+
+	RootCmd.Flags().String("log-format", DefaultLogFormat, "log format")
+	RootCmd.Flags().String("log-output", DefaultLogOutput, "log output path")
+	RootCmd.Flags().String("log-level", DefaultLogLevel, "log level")
+	RootCmd.Flags().Int("port", DefaultPort, "port number")
+	RootCmd.Flags().String("path", DefaultPath, "index directory path")
+	RootCmd.Flags().String("index-mapping", DefaultIndexMapping, "index mapping path")
+	RootCmd.Flags().String("index-type", DefaultIndexType, "index type")
+	RootCmd.Flags().String("kvstore", DefaultKvstore, "kvstore")
+	RootCmd.Flags().String("kvconfig", DefaultKvconfig, "kvconfig path")
+	RootCmd.Flags().Bool("delete-index-at-startup", DefaultDeleteIndexAtStartup, "delete index at startup")
+	RootCmd.Flags().Bool("delete-index-at-shutdown", DefaultDeleteIndexAtShutdown, "delete index at shutdown")
 
 	viper.BindPFlag("config", RootCmd.PersistentFlags().Lookup("config"))
+	viper.BindPFlag("log_format", RootCmd.Flags().Lookup("log-format"))
+	viper.BindPFlag("log_output", RootCmd.Flags().Lookup("log-output"))
+	viper.BindPFlag("log_level", RootCmd.Flags().Lookup("log-level"))
+	viper.BindPFlag("port", RootCmd.Flags().Lookup("port"))
+	viper.BindPFlag("path", RootCmd.Flags().Lookup("path"))
+	viper.BindPFlag("index_mapping", RootCmd.Flags().Lookup("index-mapping"))
+	viper.BindPFlag("index_type", RootCmd.Flags().Lookup("index-type"))
+	viper.BindPFlag("kvstore", RootCmd.Flags().Lookup("kvstore"))
+	viper.BindPFlag("kvconfig", RootCmd.Flags().Lookup("kvconfig"))
+	viper.BindPFlag("delete_index_at_startup", RootCmd.Flags().Lookup("delete-index-at-startup"))
+	viper.BindPFlag("delete_index_at_shutdown", RootCmd.Flags().Lookup("delete-index-at-shutdown"))
 }
